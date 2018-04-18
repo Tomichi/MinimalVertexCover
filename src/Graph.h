2f2f2dc21ddc08095c8d4b02b03377fdd6986bb3
Tomáš Michna <Michna12@gmail.com>
2018-04-18 19:40:07 +0200

    close #5 add basic GraphClass (#6)

---
+++ src/Graph.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include<vector>
+
+class Graph{
+	private:
+		int vertices = { 0 };
+		int edges;
+		std::vector<int> * data;
+	public:
+		Graph(const int v, const int e) : vertices{v}, edges{e} {
+			if (vertices <= 0) {
+				throw "Vertices must be postive and greater than zero.\n";
+			}
+			data = new std::vector<int>[vertices];
+		}
+
+		~Graph() {
+			delete [] data;
+		}
+
+		void addEdge(const int vertex1, const int vertex2) {
+			if (vertex1 >= vertices || vertex2 >= vertices) {
+				return;
+			}
+			data[vertex1].push_back(vertex2);
+			data[vertex2].push_back(vertex1);
+		}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "Individual.h"
+#include "Graph.h"
 
 int main() {
+	int vertices, edges, count, v1, v2;
+	std::cin >> vertices >> edges;
+	Graph graph(vertices, edges);
+	for (int i = 0; i < vertices; i++) {
+		// load vertex
+		std::cin >> v1 >> count;
+		// load edges
+		for (int j = 0; i < count; ++j) {
+			std::cin >> v2;
+			graph.addEdge(v1, v2);
+		}
+	}
+
+
+
+
 
 	Individual a(5);
 	Individual b(5);
