7d127b19da3f9d8bfc0f9689569bd25ccfd27180
Tomáš Michna <Michna12@gmail.com>
2018-04-19 21:22:23 +0200

    close  #8 Implement population class (#9)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MinimalVertexCover
 This is a project for subject PDS (Parallel distributed systems)
+```bash
+cmake .
+make
+./MinimalVertexCover < inputs/frb30-13-1.txt
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 #include "Individual.h"
 #include "Graph.h"
+#include "Population.h"
 
 int main() {
 	int vertices, edges, count, v1, v2;
 	std::cin >> vertices >> edges;
-	Graph graph(vertices, edges);
+	Graph * graph = new Graph(vertices, edges);
 	for (int i = 0; i < vertices; i++) {
 		// load vertex
 		std::cin >> v1 >> count;
 		// load edges
 		for (int j = 0; j < count; ++j) {
 			std::cin >> v2;
-			graph.addEdge(v1, v2);
+			graph->addEdge(v1, v2);
 		}
 	}
 
+	int numberOfGeneration;
+	std::cin >> numberOfGeneration;
+	Population *population = new Population(graph, numberOfGeneration);
+	population->improve();
 
-
-
-
-	Individual a(5);
-	Individual b(5);
-	Individual c(a,b);
-	a.Print();
-	std::cout << '\n';
-	b.Print();
-	std::cout << '\n';
-	c.Print();
-
+	delete population;
+	delete graph;
+	std::cout << "END";
 	return 0;
 }
 
